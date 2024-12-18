c0aba3f4fdc4e4e59b6bb919aeede57f3e776681
SyedHasibRahman <syedhasib01@gmail.com>
2024-12-18 15:47:54 +0600

    Change signin

--- backend/controller/user/userSignin.js
+++ backend/controller/user/userSignin.js
@@ -4,58 +4,65 @@ const jwt = require('jsonwebtoken');
 
 async function userSignInController(req, res) {
     try {
-        const { email, password } = req.body;
+        const { email, password } = req.body
 
         if (!email) {
-            throw new Error("Please provide email");
+            throw new Error("Please provide email")
         }
         if (!password) {
-            throw new Error("Please provide password");
+            throw new Error("Please provide password")
         }
 
-        const user = await userModel.findOne({ email });
+        const user = await userModel.findOne({ email })
 
         if (!user) {
-            throw new Error("User not found");
+            throw new Error("User not found")
         }
 
-        const checkPassword = await bcrypt.compare(password, user.password);
+        const checkPassword = await bcrypt.compare(password, user.password)
+
+        console.log("checkPassoword", checkPassword)
 
         if (checkPassword) {
             const tokenData = {
                 _id: user._id,
                 email: user.email,
-            };
-
-            const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, { expiresIn: '1y' });
+            }
+            const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, { expiresIn: 31536000 });
 
             const tokenOption = {
                 httpOnly: true,
-                secure: process.env.NODE_ENV === "production", // Secure in production
-                sameSite: "None", // Enables cross-origin cookies
-                domain: ".othooy.com", // If using subdomains
-            };
+                secure: process.env.NODE_ENV === 'production',  // Set to true in production, false in development
+                sameSite: 'None',  // Allows cross-origin cookies
+                domain: '.othooy.com',  // Set the domain to your main domain or root domain
+            }
 
-            // Set the token cookie
-            res.cookie("token", token, tokenOption)
-                .status(200)
-                .json({
-                    message: "Login successfully",
-                    data: token,
-                    success: true,
-                    error: false,
-                });
+
+            res.cookie("token", token, tokenOption).status(200).json({
+                message: "Login successfully",
+                data: token,
+                success: true,
+                error: false
+            })
 
         } else {
-            throw new Error("Invalid password");
+            throw new Error("Please check Password")
         }
+
+
+
+
+
+
+
     } catch (err) {
-        res.status(400).json({
+        res.json({
             message: err.message || err,
             error: true,
             success: false,
-        });
+        })
     }
+
 }
 
 module.exports = userSignInController
